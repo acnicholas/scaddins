d157bd1f5d1adf4ce00019537e3b30769a6b4647
Andrew Nicholas <andrewnicholas@iinet.net.au>
2015-04-24 08:32:49 +1000

    Custom revision tool BumpRevision added.

--- Build.bat
+++ Build.bat
@@ -5,7 +5,7 @@ mkdir var\log
 set SWITCHES=/consoleloggerparameters:Summary;NoItemAndPropertyList;Verbosity=normal /nologo
 set msBuildDir=%WINDIR%\Microsoft.NET\Framework\v4.0.30319
 
-call .\bin\UpdateVersion.exe -b Fixed -r Increment -i .\src\Properties\AssemblyInfo.cs -o .\src\Properties\AssemblyInfo.cs
+call .\src\BumpRevision\bin\Release\BumpRevision.exe .\src\Properties\AssemblyInfo.cs
 
 call %msBuildDir%\msbuild.exe %SWITCHES% /property:Configuration="Release2014" /property:Platform="x64" /target:Clean,Build SCaddins.csproj /l:FileLogger,Microsoft.Build.Engine;logfile=var\log\MSBuild_Release2014.log
 call %msBuildDir%\msbuild.exe %SWITCHES% /property:Configuration="Release2015" /property:Platform="x64" /target:Clean,Build SCaddins.csproj /l:FileLogger,Microsoft.Build.Engine;logfile=var\log\MSBuild_Release2015.log
