9a852b11ccbc40a696c2db72d98f5747d06f8154
Andrew Nicholas <andrewnicholas@iinet.net.au>
2015-04-24 12:45:51 +1000

    Check for updates on start-up set as default.

--- src/Scaddins.Designer.cs
+++ src/Scaddins.Designer.cs
@@ -41,7 +41,7 @@ namespace SCaddins {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool UpgradeCheckOnStartUp {
             get {
                 return ((bool)(this["UpgradeCheckOnStartUp"]));
