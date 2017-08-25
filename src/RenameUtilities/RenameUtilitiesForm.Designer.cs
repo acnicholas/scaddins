650c1c9b9f1abf6acd23736e2b9c60383f7c50b2
Andrew Nicholas <andrewnicholas@iinet.net.au>
2017-08-25 17:59:28 +1000

    Initial Rename Tools commit.

---
+++ src/RenameUtilities/RenameUtilitiesForm.Designer.cs
@@ -0,0 +1,242 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: andrewn
+ * Date: 25/08/17
+ * Time: 2:43 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace SCaddins.RenameUtilities
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Designer variable used to keep track of non-visual components.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.GroupBox grpParameter;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox grpCategory;
+        private System.Windows.Forms.GroupBox grpRename;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBoxReplace;
+        private System.Windows.Forms.TextBox textBoxFind;
+        private System.Windows.Forms.ComboBox comboBoxPresets;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Old;
+        private System.Windows.Forms.DataGridViewTextBoxColumn New;
+        
+        /// <summary>
+        /// Disposes resources used by the form.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing) {
+                if (components != null) {
+                    components.Dispose();
+                }
+            }
+            base.Dispose(disposing);
+        }
+        
+        /// <summary>
+        /// This method is required for Windows Forms designer support.
+        /// Do not change the method contents inside the source code editor. The Forms designer might
+        /// not be able to load this method if it was changed manually.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.grpParameter = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.grpCategory = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.grpRename = new System.Windows.Forms.GroupBox();
+            this.textBoxReplace = new System.Windows.Forms.TextBox();
+            this.textBoxFind = new System.Windows.Forms.TextBox();
+            this.comboBoxPresets = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Old = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.New = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.grpParameter.SuspendLayout();
+            this.grpCategory.SuspendLayout();
+            this.grpRename.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.splitContainer1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(651, 438);
+            this.panel1.TabIndex = 0;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.grpParameter);
+            this.splitContainer1.Panel1.Controls.Add(this.grpCategory);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.grpRename);
+            this.splitContainer1.Size = new System.Drawing.Size(651, 438);
+            this.splitContainer1.SplitterDistance = 255;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // grpParameter
+            // 
+            this.grpParameter.Controls.Add(this.listBox1);
+            this.grpParameter.Location = new System.Drawing.Point(3, 59);
+            this.grpParameter.Name = "grpParameter";
+            this.grpParameter.Size = new System.Drawing.Size(249, 376);
+            this.grpParameter.TabIndex = 1;
+            this.grpParameter.TabStop = false;
+            this.grpParameter.Text = "2. Parameter";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(6, 19);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(237, 355);
+            this.listBox1.TabIndex = 0;
+            // 
+            // grpCategory
+            // 
+            this.grpCategory.Controls.Add(this.comboBox1);
+            this.grpCategory.Location = new System.Drawing.Point(0, 0);
+            this.grpCategory.Name = "grpCategory";
+            this.grpCategory.Size = new System.Drawing.Size(256, 53);
+            this.grpCategory.TabIndex = 0;
+            this.grpCategory.TabStop = false;
+            this.grpCategory.Text = "1. Category";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(0, 19);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(256, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // grpRename
+            // 
+            this.grpRename.Controls.Add(this.textBoxReplace);
+            this.grpRename.Controls.Add(this.textBoxFind);
+            this.grpRename.Controls.Add(this.comboBoxPresets);
+            this.grpRename.Controls.Add(this.dataGridView1);
+            this.grpRename.Location = new System.Drawing.Point(3, 3);
+            this.grpRename.Name = "grpRename";
+            this.grpRename.Size = new System.Drawing.Size(386, 432);
+            this.grpRename.TabIndex = 0;
+            this.grpRename.TabStop = false;
+            this.grpRename.Text = "3. Rename";
+            // 
+            // textBoxReplace
+            // 
+            this.textBoxReplace.Location = new System.Drawing.Point(192, 43);
+            this.textBoxReplace.Name = "textBoxReplace";
+            this.textBoxReplace.Size = new System.Drawing.Size(188, 20);
+            this.textBoxReplace.TabIndex = 3;
+            // 
+            // textBoxFind
+            // 
+            this.textBoxFind.Location = new System.Drawing.Point(6, 43);
+            this.textBoxFind.Name = "textBoxFind";
+            this.textBoxFind.Size = new System.Drawing.Size(180, 20);
+            this.textBoxFind.TabIndex = 2;
+            // 
+            // comboBoxPresets
+            // 
+            this.comboBoxPresets.FormattingEnabled = true;
+            this.comboBoxPresets.Location = new System.Drawing.Point(6, 16);
+            this.comboBoxPresets.Name = "comboBoxPresets";
+            this.comboBoxPresets.Size = new System.Drawing.Size(180, 21);
+            this.comboBoxPresets.TabIndex = 1;
+            this.comboBoxPresets.SelectedValueChanged += new System.EventHandler(this.ComboBoxPresetsSelectedValueChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Old,
+            this.New});
+            this.dataGridView1.Location = new System.Drawing.Point(6, 75);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(383, 357);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(588, 461);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 461);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Old
+            // 
+            this.Old.HeaderText = "Old";
+            this.Old.Name = "Old";
+            // 
+            // New
+            // 
+            this.New.HeaderText = "New";
+            this.New.Name = "New";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(675, 496);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Rename";
+            this.panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.grpParameter.ResumeLayout(false);
+            this.grpCategory.ResumeLayout(false);
+            this.grpRename.ResumeLayout(false);
+            this.grpRename.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+    }
+}
---
+++ src/RenameUtilities/RenameUtilitiesForm.cs
@@ -0,0 +1,128 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: andrewn
+ * Date: 25/08/17
+ * Time: 2:43 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace SCaddins.RenameUtilities
+{
+    /// <summary>
+    /// Description of Form1.
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            //
+            // The InitializeComponent() call is required for Windows Forms designer support.
+            //
+            InitializeComponent();
+            PopulateCategoryComboBox();
+            PopulatePresetsComboBox();
+            dataGridView1.Rows[0].Cells[0].Value = "test";
+            
+            //
+            // TODO: Add constructor code after the InitializeComponent() call.
+            //
+        }
+        
+        private void PopulateCategoryComboBox()
+        {
+            comboBox1.Items.Add("Rooms");
+            comboBox1.Items.Add("Generic Annotations");
+            comboBox1.Items.Add("Views");
+            comboBox1.Items.Add("Sheets");
+            comboBox1.Items.Add("Revisions");
+            comboBox1.Items.Add("Walls");
+            comboBox1.Items.Add("Doors");
+            comboBox1.Items.Add("Floors");
+            comboBox1.Items.Add("Roofs");
+        }
+        
+        private void PopulatePresetsComboBox()
+        {
+            comboBoxPresets.Items.Add("Custom");
+            comboBoxPresets.Items.Add("Uppercase");
+            comboBoxPresets.Items.Add("Lowercase");
+            comboBoxPresets.Items.Add("TitleCase");
+            comboBoxPresets.Items.Add("Lazy Increment");
+            comboBoxPresets.Items.Add("Smart Increment");
+            comboBoxPresets.Items.Add("Mirror");
+        }
+               
+        void ComboBox1SelectedIndexChanged(object sender, EventArgs e)
+        {
+          
+        }
+        
+        void Panel2Paint(object sender, PaintEventArgs e)
+        {
+          
+        }
+        
+        void Panel1Paint(object sender, PaintEventArgs e)
+        {
+          
+        }
+        
+        void Button3Click(object sender, EventArgs e)
+        {
+          
+        }
+        
+        private static string GetReplacementResult(string s, string pattern, string replacement)
+        {
+            return Regex.Replace(s, pattern, replacement);
+        }
+        
+        void DataGridToUpper()
+        {
+            dataGridView1.Rows[0].Cells[1].Value = "sdgsdfg";
+            //dataGridView1.Rows[0].Cells[1].Value = dataGridView1.Rows[0].Cells[0].Value.ToString().ToUpper();
+        }
+        
+        void UpdateDataGrid()
+        {
+            dataGridView1.Rows[0].Cells[1].Value = 
+                GetReplacementResult(dataGridView1.Rows[0].Cells[0].Value.ToString(), textBoxFind.Text, textBoxReplace.Text);
+        }
+        
+        void ClearFindAndReplace()
+        {
+            textBoxFind.Text = string.Empty;
+            textBoxReplace.Text = string.Empty;    
+        }
+        
+        void EnableFindAndReplace(bool enable)
+        {
+            textBoxFind.Enabled = enable;
+            textBoxReplace.Enabled = enable;  
+            ClearFindAndReplace();
+        }
+        
+        void ComboBoxPresetsSelectedValueChanged(object sender, EventArgs e)
+        {
+            if(comboBoxPresets.Text == "Custom") {
+                EnableFindAndReplace(true);
+                UpdateDataGrid();
+            }
+            if(comboBoxPresets.Text == "Uppercase") {
+                EnableFindAndReplace(false);
+                DataGridToUpper();
+            }
+            if(comboBoxPresets.Text == "Lowercase") {
+                textBoxFind.Text = @"^.*$";
+                textBoxReplace.Text = @"\L($1)\E";
+                UpdateDataGrid();
+            }
+            //UpdateDataGrid();
+        }
+    }
+}
