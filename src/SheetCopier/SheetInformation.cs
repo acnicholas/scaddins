74e705d00e54646bb3ef8d310337e44dc058b58b
Andrew Nicholas <andrewn@scottcarver.com.au>
2018-03-09 17:14:45 +1100

    Added SheetCopier/SheetInformation.cs

---
+++ src/SheetCopier/SheetInformation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+
+namespace SCaddins.SheetCopier
+{
+    class SheetInformation
+    {
+        public string ParameterName
+        {
+            get; private set;
+        }
+
+        public string ParameterValue
+        {
+            get; private set;
+        }
+
+        public SheetInformation(Parameter param)
+        {
+            ParameterName = param.Definition.Name;
+            switch (param.StorageType) {
+                case StorageType.Double:
+                    ParameterValue = param.AsString();
+                    break;
+                case StorageType.ElementId:
+                    ParameterValue = param.AsString();
+                break;
+                case StorageType.Integer:
+                    ParameterValue = param.AsInteger().ToString();
+                    break;
+                case StorageType.None:
+                    ParameterValue = string.Empty;
+                    break;
+                case StorageType.String:
+                ParameterValue = param.AsString();
+                    break;
+                default:
+                    ParameterValue = param.AsString();
+                break;
+            }           
+        }
+    }
+}
