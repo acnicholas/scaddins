76972a098a8ce7e592ed04a48508b6eaa2d5586f
Andrew Nicholas <andrewn@scottcarver.com.au>
2018-03-14 10:21:14 +1100

    Added missing xaml files for RoomConvertor and SolarUtilities.

---
+++ src/SolarUtilities/ViewModels/SolarViewsViewModel.cs
@@ -0,0 +1,78 @@
+﻿namespace SCaddins.SolarUtilities.ViewModels
+{
+    using Autodesk.Revit.UI;
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using Caliburn.Micro;
+
+    class SolarViewsViewModel : PropertyChangedBase
+    {
+        private SolarViews model;
+
+        public SolarViewsViewModel(UIDocument uidoc)
+        {
+            model = new SolarViews(uidoc);
+        }
+       
+        public bool RotateCurrentView
+        {
+            get { return model.RotateCurrentView; }
+            set
+            {
+                if (model.RotateCurrentView != value) {
+                    model.RotateCurrentView = value;
+                }
+            }
+        }
+
+        public bool CanRotateCurrentView
+        {
+            get
+            {
+                return model.CanRotateActiveView;
+            }
+        }
+        
+        public bool Create3dViews {
+            get { return model.Create3dViews; }
+            set
+            {
+                if (model.Create3dViews != value) {
+                    model.Create3dViews = value;
+                }
+            }
+        }
+
+        //public DateTime Time
+        //{
+        //    get
+        //}
+        
+        public bool CreateShadowPlans
+        {
+            get { return model.CreateShadowPlans; }
+            set
+            {
+                if (model.CreateShadowPlans != value) {
+                    model.CreateShadowPlans = value;
+                }
+            }
+        }
+
+        public string ViewInformation
+        {
+            get
+            {
+                //return "test";
+                return model.ActiveIewInformation;
+            }
+        }
+                              
+        public TimeSpan ExportTimeInterval {
+            get;
+            set;
+        }
+
+    }
+}
---
+++ src/SolarUtilities/Views/SolarViewsView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace SCaddins.SolarUtilities.Views
+{
+    /// <summary>
+    /// Interaction logic for SolarViewsForm.xaml
+    /// </summary>
+    public partial class SolarViewsView : UserControl
+    {
+        public SolarViewsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
