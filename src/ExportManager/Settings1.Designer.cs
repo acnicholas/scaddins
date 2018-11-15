d717176e75fef2ee8d38a4df22a85e576626c817
Andrew Nicholas <andrewnicholas@iinet.net.au>
2018-11-15 21:03:53 +1100

    Force Raster option added to settings file.

--- src/ExportManager/Settings1.Designer.cs
+++ src/ExportManager/Settings1.Designer.cs
@@ -238,5 +238,17 @@ namespace SCaddins.ExportManager {
                 this["UseDateForEmptyRevisions"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("Force Raster Print")]
+        public string UseRasterPrinterParameter {
+            get {
+                return ((string)(this["UseRasterPrinterParameter"]));
+            }
+            set {
+                this["UseRasterPrinterParameter"] = value;
+            }
+        }
     }
 }
