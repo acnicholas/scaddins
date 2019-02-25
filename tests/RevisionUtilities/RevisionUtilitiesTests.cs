b0a48cd23dedea767e6f3b03fa468bf95ceb935f
Andrew Nicholas <andrewnicholas@iinet.net.au>
2019-02-25 20:23:46 +1100

    models/scaddins_test_model.rvt added. Some tests added.

--- tests/RevisionUtilities/RevisionUtilitiesTests.cs
+++ tests/RevisionUtilities/RevisionUtilitiesTests.cs
@@ -1,10 +1,14 @@
-﻿using NUnit.Framework;
+﻿using Autodesk.Revit.DB;
+using NUnit.Framework;
+using RTF.Applications;
+using RTF.Framework;
 using SCaddins.RevisionUtilities;
-using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System;
 
 namespace SCaddins.RevisionUtilities.Tests
 {
@@ -12,27 +16,41 @@ namespace SCaddins.RevisionUtilities.Tests
     public class RevisionUtilitiesTests
     {
         [Test()]
+        [TestModel(@"./scaddins_test_model.rvt")]
         public void GetRevisionCloudsTest()
         {
-            Assert.Fail();
+            var doc = RevitTestExecutive.CommandData.Application.ActiveUIDocument.Document;
+            var clouds = Manager.GetRevisionClouds(doc);
+            Assert.IsTrue(clouds.Count == 5);
         }
 
         [Test()]
+        [TestModel(@"./scaddins_test_model.rvt")]
         public void GetRevisionsTest()
         {
-            Assert.Fail();
+            var doc = RevitTestExecutive.CommandData.Application.ActiveUIDocument.Document;
+            var revisions = Manager.GetRevisions(doc);
+            Assert.IsTrue(revisions.Count == 5);
         }
 
         [Test()]
+        [TestModel(@"./scaddins_test_model.rvt")]
         public void AssignRevisionToCloudsTest()
         {
-            Assert.Fail();
+            var doc = RevitTestExecutive.CommandData.Application.ActiveUIDocument.Document;
+            var revision = Manager.GetRevisions(doc).Where(r => r.Description == "DA Issue").First();
+            Manager.AssignRevisionToClouds(doc, Manager.GetRevisionClouds(doc), revision.Id);
+            var count = Manager.GetRevisionClouds(doc).Where(c => c.Revision.Equals(revision)).Count();
+            Assert.IsTrue(count == 5);
         }
 
         [Test()]
+        [TestModel(@"./scaddins_test_model.rvt")]
         public void DeleteRevisionCloudsTest()
         {
-            Assert.Fail();
+            var doc = RevitTestExecutive.CommandData.Application.ActiveUIDocument.Document;
+            Manager.DeleteRevisionClouds(doc, Manager.GetRevisionClouds(doc));
+            Assert.IsTrue(Manager.GetRevisionClouds(doc).Count == 0);
         }
     }
 }
--- tests/RoomConvertor/RoomConversionManagerTests.cs
+++ tests/RoomConvertor/RoomConversionManagerTests.cs
@@ -24,37 +24,76 @@ namespace SCaddins.RoomConvertor.Tests
     public class RoomConversionManagerTests
     {
         [Test()]
+        [TestModel(@"./scaddins_test_model.rvt")]
         public void CreateViewsAndSheetsTest()
         {
-            Assert.Fail();
+            var doc = RevitTestExecutive.CommandData.Application.ActiveUIDocument.Document;
+            var origViews = new FilteredElementCollector(doc)
+                .OfClass(typeof(View))
+                .ToElements().Count;
+            var manager = new RoomConversionManager(doc);
+            manager.CreateViewsAndSheets(manager.Candidates);
+            var newViews = new FilteredElementCollector(doc)
+    .           OfClass(typeof(View))
+                .ToElements().Count;
+            Assert.IsTrue(newViews - origViews == 16);
         }
 
         [Test()]
-        [TestModel(@"./rac_basic_sample_project.rvt")]
+        [TestModel(@"./scaddins_test_model.rvt")]
         public void GetAllTitleBlockTypesTest()
         {
             var doc = RevitTestExecutive.CommandData.Application.ActiveUIDocument.Document;
-            Assert.IsTrue(RoomConversionManager.GetAllTitleBlockTypes(doc).Count == 4);
+            // Allow for an empty title block in this count
+            Assert.IsTrue(RoomConversionManager.GetAllTitleBlockTypes(doc).Count == 5);
         }
 
         [Test()]
+        [TestModel(@"./scaddins_test_model.rvt")]
         public void GetTitleBlockByNameTest()
         {
-            Assert.Fail();
+            var doc = RevitTestExecutive.CommandData.Application.ActiveUIDocument.Document;
+            var manager = new RoomConversionManager(doc);
+            Assert.IsTrue(manager.GetTitleBlockByName("A0 metric-A0 metric") != ElementId.InvalidElementId);
+            Assert.IsTrue(manager.GetTitleBlockByName("a2-Family3") != ElementId.InvalidElementId);
+            //Assert.IsTrue(manager.GetTitleBlockByName(null) == ElementId.InvalidElementId);
+            Assert.IsTrue(manager.GetTitleBlockByName(@"!@#$%^&*(){}[]") == ElementId.InvalidElementId);
         }
 
         [Test()]
+        [TestModel(@"./scaddins_test_model.rvt")]
         public void CreateRoomMassesTest()
+        {
+            var doc = RevitTestExecutive.CommandData.Application.ActiveUIDocument.Document;
+            var origMasses = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_Mass)
+                .OfClass(typeof(DirectShape))
+                .ToElements().Count;
+            var manager = new RoomConversionManager(doc);
+            manager.CreateRoomMasses(manager.Candidates);
+            var fec = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Mass).OfClass(typeof(DirectShape));
+            var newMasses = fec.ToElements().Count - origMasses;
+            Assert.IsTrue(newMasses == 8);
+        }
+
+        [Test()]
+        [TestModel(@"./scaddins_test_model.rvt")]
+        public void SynchronizeMassesToRoomsTest()
         {
             var doc = RevitTestExecutive.CommandData.Application.ActiveUIDocument.Document;
             var manager = new RoomConversionManager(doc);
             manager.CreateRoomMasses(manager.Candidates);
-            Assert.Fail();
-        }
-
-        [Test()]
-        public void SynchronizeMassesToRoomsTest()
-        {
+            var fec = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Mass).OfClass(typeof(DirectShape));
+            foreach (Element e in fec) {
+                var param = e.LookupParameter("Department");
+                if (param != null && param.AsString() != "Test") {
+                    param.Set("Test");
+                } else {
+                    Assert.Fail();
+                }
+            }
+            manager.SynchronizeMassesToRooms();
+            //TIDO
             Assert.Fail();
         }
     }
