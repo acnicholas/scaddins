014347065385706d6e96a06232c4bc9cee4c4ce7
Andrew Nicholas <andrewn@scottcarver.com.au>
2019-04-26 17:07:49 +1000

    Initial Hatch Editor Commit.

---
+++ src/HatchEditor/HatchEditor.cs
@@ -0,0 +1,84 @@
+﻿namespace SCaddins.HatchEditor
+{
+    using System.Collections.Generic;
+    using System.Dynamic;
+    using System.Linq;
+    using System.Text;
+    using Autodesk.Revit.UI;
+    using Autodesk.Revit.DB;
+
+    public static class ExtensionMethods
+    {
+        public static double ToMM(this double arg)
+        {
+            return System.Math.Round(arg * 304.8, 8);
+        }
+
+        public static double ToDeg(this double arg)
+        {
+            return System.Math.Round(arg * 180 / System.Math.PI, 8);
+        }
+    }
+
+    class HatchEditor
+    {
+
+        [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+        [Autodesk.Revit.Attributes.Regeneration(Autodesk.Revit.Attributes.RegenerationOption.Manual)]
+        [Autodesk.Revit.Attributes.Journaling(Autodesk.Revit.Attributes.JournalingMode.NoCommandData)]
+        public class Command : IExternalCommand
+        {
+            public Autodesk.Revit.UI.Result Execute(
+                ExternalCommandData commandData,
+                ref string message,
+                Autodesk.Revit.DB.ElementSet elements)
+            {
+                if (commandData == null)
+                {
+                    return Result.Failed;
+                }
+
+                UIDocument udoc = commandData.Application.ActiveUIDocument;
+
+                dynamic settings = new ExpandoObject();
+                settings.Height = 480;
+                settings.Width = 768;
+                settings.Title = "Hatch Editor - By Andrew Nicholas";
+                settings.ShowInTaskbar = false;
+                settings.SizeToContent = System.Windows.SizeToContent.Manual;
+
+                var vm = new ViewModels.HatchEditorViewModel(udoc.Document);
+                SCaddinsApp.WindowManager.ShowDialog(vm, null, settings);
+                return Result.Succeeded;
+            }
+
+            public static List<FillPattern> FillPatterns(Document doc)
+            {
+                var result = new List<FillPattern>();
+                using (var f = new FilteredElementCollector(doc)) {
+                    f.OfClass(typeof(FillPatternElement));
+                    foreach (FillPatternElement e in f)
+                    {
+                        result.Add(e.GetFillPattern());
+                    }
+                }
+                //Autodesk.Revit.UI.TaskDialog.Show("test", result.Count.ToString());
+                return result;
+            }
+
+
+
+            public static string GetPatternDefinition(FillPattern pattern)
+            {
+                StringBuilder s = new StringBuilder();
+                foreach (var p in pattern.GetFillGrids())
+                {
+                    s.Append(string.Format("{0},\t{1},\t{2},\t{3},\t{4}", p.Angle.ToDeg(), p.Origin.U.ToMM(), p.Origin.V.ToMM(), p.Offset.ToMM(), p.Shift.ToMM()));
+                    s.Append(System.Environment.NewLine);
+                }
+                return s.ToString();
+            }
+
+        }
+    }
+}
---
+++ src/HatchEditor/ViewModels/HatchEditorViewModel.cs
@@ -0,0 +1,76 @@
+﻿namespace SCaddins.HatchEditor.ViewModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using Autodesk.Revit.DB;
+    using Caliburn.Micro;
+
+    internal class HatchEditorViewModel : Screen
+    {
+        private Document doc;
+        private ObservableCollection<FillPattern> fillPattensInModel;
+        private FillPattern selectedFillPattern;
+        private string currentPatternDefinition;
+
+        public HatchEditorViewModel(Autodesk.Revit.DB.Document doc)
+        {
+            this.doc = doc;
+            FillPatterns = new ObservableCollection<FillPattern>(HatchEditor.Command.FillPatterns(doc));
+            CurrentPatternDefinition = "test";
+        }
+
+        public ObservableCollection<FillPattern> FillPatterns
+        {
+            get
+            {
+                return fillPattensInModel;
+            }
+
+            set
+            {
+                fillPattensInModel = value;
+                NotifyOfPropertyChange(() => FillPatterns);
+            }
+        }
+
+        public FillPattern SelectedFillPattern
+        {
+            get
+            {
+                return selectedFillPattern;
+            }
+
+            set
+            {
+                selectedFillPattern = value;
+                CurrentPatternDefinition = HatchEditor.Command.GetPatternDefinition(SelectedFillPattern);
+                NotifyOfPropertyChange(() => SelectedFillPattern);
+                NotifyOfPropertyChange(() => CurrentPatternType);
+            }
+        }
+
+        public string CurrentPatternDefinition
+        {
+            get
+            {
+                return currentPatternDefinition;
+            }
+
+            set
+            {
+                currentPatternDefinition = value;
+                NotifyOfPropertyChange(() => CurrentPatternDefinition);
+            }
+        }
+
+        public FillPatternTarget CurrentPatternType
+        {
+            get
+            {
+                return selectedFillPattern.Target;
+            } 
+        }
+
+    }
+}
