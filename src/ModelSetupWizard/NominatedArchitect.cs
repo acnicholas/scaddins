d6f6771eef0ca5645e4bd347ed5eebb6f38e3e7c
Andrew Nicholas <andrewnicholas@iinet.net.au>
2019-06-03 21:12:11 +1000

    Add missing NominatedArchitect file.

---
+++ src/ModelSetupWizard/NominatedArchitect.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCaddins.ModelSetupWizard
+{
+    class NominatedArchitect
+    {
+        public NominatedArchitect(string name, string id)
+        {
+            Name = name;
+            Id = id;
+        }
+
+        public string Id {
+            get; set;
+        }
+
+        public string Name {
+            get; set;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0};{1}", Name, Id);
+        }
+
+
+    }
+}
