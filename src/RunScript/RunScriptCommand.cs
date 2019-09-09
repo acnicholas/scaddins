f8c12637002f10a9366f3585c1a67142fc95a691
Andrew Nicholas <andrewn@scottcarver.com.au>
2019-09-09 16:28:07 +1000

    Initial test at running cs-script scripts.

---
+++ src/RunScript/RunScriptCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace SCaddins.RunScript
+{
+    using Autodesk.Revit.DB;
+    using Autodesk.Revit.UI;
+    using Autodesk.Revit.Attributes;
+    using CSScriptLibrary;
+
+    [Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    [Regeneration(Autodesk.Revit.Attributes.RegenerationOption.Manual)]
+    [Journaling(Autodesk.Revit.Attributes.JournalingMode.NoCommandData)]
+    public class RunScriptCommand : IExternalCommand
+    {
+        public Result Execute(
+            ExternalCommandData commandData,
+            ref string message,
+            ElementSet elements)
+        {
+            if (commandData == null)
+            {
+                return Result.Failed;
+            }
+
+            var vm = new ViewModels.RunScriptViewModel();
+            SCaddinsApp.WindowManager.ShowDialog(vm, null, ViewModels.RunScriptViewModel.DefaultViewSettings);
+
+            RunScript(commandData.Application.ActiveUIDocument.Document, vm.Script);
+
+            return Result.Succeeded;
+        }
+
+        public static void RunScript(Document doc, string script)
+        {
+            string[] assemblies = { @"C:\Program Files\Autodesk\Revit 2020\RevitAPI.dll", @"C:\Program Files\Autodesk\Revit 2020\RevitAPIUI.dll" };
+            var RunScript = CSScript.LoadMethod(script, assemblies).GetStaticMethod("*.Main", doc, script);
+            RunScript(doc, script);
+        }
+    }
+}
---
+++ src/RunScript/ViewModels/RunScriptViewModel.cs
@@ -0,0 +1,77 @@
+﻿
+// (C) Copyright 2019 by Andrew Nicholas
+//
+// This file is part of SCaddins.
+//
+// SCaddins is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// SCaddins is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with SCaddins.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace SCaddins.RunScript.ViewModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Dynamic;
+    using Autodesk.Revit.DB;
+    using Autodesk.Revit.UI;
+    using Caliburn.Micro;
+
+    internal class RunScriptViewModel : Screen
+    {
+        public RunScriptViewModel()
+        {
+            Script =
+@"
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB; 
+
+public static void Main(Document doc, string script)
+{
+    using (var t = new Transaction(doc)) {
+        t.Start(""Run Script"");
+        var fec = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Rooms);
+            foreach (var r in fec)
+            {
+                var room = r as Autodesk.Revit.DB.Architecture.Room;
+                room.Name = room.Name.ToUpper();
+            }
+            t.Commit();
+        }
+}
+";
+        }
+
+        public static dynamic DefaultViewSettings
+        {
+            get
+            {
+                dynamic settings = new ExpandoObject();
+                settings.Height = 480;
+                settings.Width = 300;
+                settings.Title = "Run (cs)Script";
+                settings.ShowInTaskbar = false;
+                settings.SizeToContent = System.Windows.SizeToContent.WidthAndHeight;
+                return settings;
+            }
+        }
+
+        public string Script
+        {
+            get; set;
+        }
+
+        //public void Run()
+        //{
+        //    RunScript.
+        //}
+    }
+}
