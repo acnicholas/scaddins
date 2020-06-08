68535f3e76c17301ee06c7d4cd47cfa601c5b9f6
Andrew Nicholas <andrewn@scottcarver.com.au>
2020-06-08 21:41:04 +1000

    Start creation of new global settings window.

---
+++ src/Common/Settings.cs
@@ -0,0 +1,22 @@
+﻿namespace SCaddins.Common
+{
+    using Autodesk.Revit.UI;
+
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    [Autodesk.Revit.Attributes.Regeneration(Autodesk.Revit.Attributes.RegenerationOption.Manual)]
+    [Autodesk.Revit.Attributes.Journaling(Autodesk.Revit.Attributes.JournalingMode.NoCommandData)]
+    public class Settings : IExternalCommand
+    {
+        public Result Execute(
+            ExternalCommandData commandData,
+            ref string message,
+            Autodesk.Revit.DB.ElementSet elements)
+        {
+
+            var vms = new ParameterUtilities.ViewModels.SCincrementViewModel();
+            var vm = new ViewModels.SettingsViewModel(vms);
+            SCaddinsApp.WindowManager.ShowDialog(vm, null, ViewModels.SettingsViewModel.DefaultWindowSettings);
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ src/Common/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCaddins.Common.ViewModels
+{
+
+    using Caliburn.Micro;
+    using System.ComponentModel.Composition;
+    using System.Diagnostics;
+    using System.Reflection;
+    using ParameterUtilities.ViewModels;
+
+    class SettingsViewModel : PropertyChangedBase
+    {
+        private SCincrementViewModel incrementViewModel;
+
+        public SettingsViewModel(SCincrementViewModel svm)
+        {
+            IncrementViewModel = svm;
+        }
+
+        public SCincrementViewModel IncrementViewModel
+        {
+            get { return incrementViewModel; }
+            set
+            {
+                incrementViewModel = value;
+                NotifyOfPropertyChange(() => IncrementViewModel);
+            }
+        }
+
+        public static dynamic DefaultWindowSettings
+        {
+            get
+            {
+                dynamic settings = new System.Dynamic.ExpandoObject();
+                settings.Height = 400;
+                settings.Title = "SCaddins Global Settings";
+                settings.ShowInTaskbar = false;
+                settings.SizeToContent = System.Windows.SizeToContent.WidthAndHeight;
+                settings.ResizeMode = System.Windows.ResizeMode.CanResize;
+                return settings;
+            }
+        }
+    }
+}
