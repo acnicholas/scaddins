f0b786be0e61479cbb23a7f83aeba3a2a717a89d
Andrew Nicholas <andrewn@scottcarver.com.au>
2020-12-17 16:47:58 +1100

    Bump revision.

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -17,5 +17,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("21.4.0.2")]
-[assembly: AssemblyInformationalVersion("21.4")]
+[assembly: AssemblyVersion("21.4.1.0")]
+[assembly: AssemblyInformationalVersion("21.4.1")]
